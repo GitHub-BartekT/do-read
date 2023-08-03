42a9c63e8226530c034d8e7a1b1781865a8d16d1
Bartlomiej Tucholski <bartlomiej.tucholski@gmail.com>
2023-08-03 12:40:01 +0200

    Create empty Spring project

---
+++ src/main/java/pl/iseebugs/read/DoReadApplication.java
@@ -0,0 +1,13 @@
+package pl.iseebugs.read;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoReadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoReadApplication.class, args);
+	}
+
+}
